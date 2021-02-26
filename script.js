b951fbef7008582861a98bf88c7bf96bcf11fa5c
Aljon <aljon_mendiola799224@yahoo.com>
2021-02-26 17:23:56 +0800

    initial commit

--- script.js
+++ script.js
@@ -399,3 +399,31 @@ fetch(dataURL)
     populateCurrenciesList();
   })
   .catch((err) => console.log(err));
+
+////////////// date now
+
+const months = new Array(12);
+months[0] = "January";
+months[1] = "February";
+months[2] = "March";
+months[3] = "April";
+months[4] = "May";
+months[5] = "June";
+months[6] = "July";
+months[7] = "August";
+months[8] = "September";
+months[9] = "October";
+months[10] = "November";
+months[11] = "December";
+
+function dateNow() {
+  const d = new Date();
+  const month = d.getMonth();
+  const day = d.getDate();
+  const year = d.getFullYear();
+  const dateNow = (document.querySelector(
+    ".date"
+  ).textContent = `${months[month]} ${day}, ${year}`);
+}
+
+dateNow();
